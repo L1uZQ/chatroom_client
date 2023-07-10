6fd2857b1d3af4a8e00647e37c664555ffb02bfe
“lzq” <lzquestc@outlook.com>
2023-07-10 17:26:13 +0800

    数据编码

--- dataencoder.cpp
+++ dataencoder.cpp
@@ -1,6 +1,27 @@
 #include "dataencoder.h"
-
+#include<QDebug>
 dataencoder::dataencoder()
 {
 
+}
+
+string dataencoder::encode(unsigned int protocolid, unsigned int account, unsigned int datatype, unsigned int datalength)
+{
+    hp = head;
+    encodeElement(protocolId, PROTOCOL_ID_SIZE);
+    encodeElement(account, ACCOUNT_SIZE);
+    encodeElement(dataType, DATA_TYPE_SIZE);
+    encodeElement(dataLength, DATA_SIZE);
+    return string(head,sizeof(head));
+}
+
+void dataencoder::encodeElement(unsigned int data, unsigned int len)
+{
+    char* c = hp + len - 1;
+    for (int i = len; i > 0; i--) {
+        *c = (char)(data & 0xff);
+        c--;
+        data >>= 8;
+    }
+    hp = hp + len;
 }
--- dataencoder.h
+++ dataencoder.h
@@ -1,11 +1,22 @@
+#pragma once
 #ifndef DATAENCODER_H
 #define DATAENCODER_H
+#include"config.h"
+#include<QString>
 
+using namespace std;
 
 class dataencoder
 {
+private:
+    char head[BASE_BUFFER_SIZE];
+    char * hp;
+    void encodeElement(unsigned int data, unsigned int len);
+
+
 public:
     dataencoder();
+    string encode(unsigned int protocolid,unsigned int account, unsigned int datatype, unsigned int datalength);
 };
 
 #endif // DATAENCODER_H
